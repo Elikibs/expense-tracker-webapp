6758c9c5523bfd115de6f195bf6ab4bb406d7440
Elisha <elishakibet67@gmail.com>
2023-10-15 13:51:25 +0300

    Updated the functionality

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-// Get reference to budget input
-const totalAmount = document.getElementById("total-amount");
-// Get reference to type of expense input
-const productTitle = document.getElementById("product-title");
-// Get reference to expense cost input
-const userAmount = document.getElementById("user-amount");
-// Get reference to set budget button
-const setBudgetButton = document.getElementById("total-amount-button");
-// Get reference to the Submit Amount button
-const submitAmountButton = document.getElementById("check-amount");
+const form = document.querySelector("form");
+const expenseName = document.querySelector("#expense-name");
+const expenseAmount = document.querySelector("#expense-amount");
+const expenseList = document.getElementById("expense-list");
+const totalAmountElement = document.getElementById("total-amount");
 
-let currentId = 1;
+let totalAmount = 0;
 
-
-setBudgetButton.addEventListener("click", () => {
-    console.log(totalAmount.value)
-    fetch("http://localhost:3000/records", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({
-            Budget: parseFloat(totalAmount.value)
-        })
+document.addEventListener("DOMContentLoaded", () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/expenses", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        expense: expenseName.value,
+        cost: parseFloat(expenseAmount.value),
+      }),
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then((response) => response.json())
+    .then((expenses) => renderExpenseList(expenses));
+    form.reset();
+  });
 
-})
- submitAmountButton.addEventListener("click", () => {
-    console.log(productTitle.value)
-    console.log(userAmount.value)
- })
+  // Event delegation to handle delete button clicks
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-btn")) {
+      const expenseId = e.target.id;
+      console.log(expenseId)
+      fetch(`http://localhost:3000/expenses/${expenseId}`, {
+        method: "DELETE",
+      })
+        .then(response => response.json())
+        .then(() => {
+          // Remove the row from the DOM
+          e.target.parentElement.remove();
+
+          // Update the totalAmount
+          const deletedExpenseCost = parseFloat(
+            e.target.previousElementSibling.textContent
+          );
+          totalAmount -= deletedExpenseCost;
+          totalAmountElement.textContent = totalAmount.toFixed(2);
+        })
+        .catch((error) => console.error("Error deleting expense:", error));
+    }
+  });
+});
+
+
+
+function renderExpenseList(expenses) {
+  const expenseRow = document.createElement("tr");
+  expenseRow.innerHTML = `
+    <td>${expenses.expense}</td>
+    <td>${expenses.cost}</td>
+    <td class="delete-btn" id="${expenses.id}">Delete</td>
+  `;
+
+  totalAmount += expenses.cost;
+  expenseList.appendChild(expenseRow);
+  totalAmountElement.textContent = totalAmount.toFixed(2);
+}
